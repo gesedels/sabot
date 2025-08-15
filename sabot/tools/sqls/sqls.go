da2ad874feaebd51ba41340c15d2fea2bd536e9a
Stephen Malone <mail@gesedels.com>
2025-08-15 14:57:12 +1000

    Added tools/sqls package.

---
+++ sabot/tools/sqls/sqls.go
@@ -0,0 +1,26 @@
+// Package sqls implements SQLite query constants.
+package sqls
+
+// Pragma is the default always-enabled database pragma.
+const Pragma = `
+	pragma encoding = 'utf-8';
+	pragma foreign_keys = on;
+`
+
+// Schema is the default first-run database schema.
+const Schema = `
+	create table Notes (
+		id   integer primary key asc,
+		init integer not null default (unixepoch()),
+		name text    not null unique,
+		hash text    not null unique
+	);
+
+	create table Pages (
+		id   integer primary key asc,
+		init integer not null default (unixepoch()),
+		note integer not null references Notes(id),
+		body text    not null,
+		hash text    not null unique
+	);
+`
