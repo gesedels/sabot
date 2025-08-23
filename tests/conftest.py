c4590b56e5f5e518e089e58cf6435729aefbf598
Stephen Malone <mail@gesedels.com>
2025-08-23 16:28:14 +1000

    Added mock database fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,16 @@ Global unit testing fixtures.
 
 import pytest
 import sqlite3
+from sabot.tools import sqls
+
+INSERTS = """
+    insert into Notes (init, name) values (1000, 'alpha');
+    insert into Notes (init, name) values (2000, 'bravo');
+
+    insert into Pages (init, note, body) values (1000, 1, 'Alpha old.' || char(10));
+    insert into Pages (init, note, body) values (1100, 1, 'Alpha new.' || char(10));
+    insert into Pages (init, note, body) values (2000, 2, 'Bravo.' || char(10));
+"""
 
 
 @pytest.fixture(scope="function")
@@ -14,4 +24,15 @@ def dbse() -> sqlite3.Connection:
 
     dbse = sqlite3.connect(":memory:")
     dbse.row_factory = sqlite3.Row
+    return dbse
+
+
+@pytest.fixture(scope="function")
+def mock(dbse: sqlite3.Connection) -> sqlite3.Connection:
+    """
+    Return a new in-memory SQLite database connection containing pragma, schema
+    and mock inserts.
+    """
+
+    dbse.executescript(sqls.PRAGMA + sqls.SCHEMA + INSERTS)
     return dbse
