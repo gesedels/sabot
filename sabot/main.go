1fd5de9257c61fecff279b7d44a688c43ce22c4d
Stephen Malone <mail@gesedels.com>
2025-09-01 12:34:19 +1000

    Added main script.

---
+++ sabot/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	comm "github.com/gesedels/sabot/sabot/comms"
+	"github.com/gesedels/sabot/sabot/items/book"
+)
+
+func try(err error) {
+	if err != nil {
+		fmt.Printf("Error: %s.\n", err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	book, err := book.NewPath("./sabot.db")
+	try(err)
+	try(comm.Run(os.Stdout, book, os.Args[1:]))
+}
